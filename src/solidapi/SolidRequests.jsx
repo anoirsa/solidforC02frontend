46d2a7deffe9c86d7ceedabe3286815ddbe1082d
anoirsa <anouarbelilawoo@outlook.com>
2021-12-06 17:38:10 +0200

    final stages

--- src/solidapi/SolidRequests.jsx
+++ src/solidapi/SolidRequests.jsx
@@ -10,6 +10,7 @@ import {
   addStringNoLocale,
   buildThing,
   getStringNoLocale,
+  removeStringNoLocale,
 } from "@inrupt/solid-client";
 import { RDF } from "@inrupt/vocab-common-rdf";
 import {
@@ -40,16 +41,16 @@ const adjustSubDataSet = async (subComponent, mainComponent) => {
       SOLID_RESOURCE_URL.SUB_COM
     );
     let thingToAdd = null;
+    let componentsThatBelongTo = null;
     if (
       !isThingExsists(
         componentSolidDataSet,
         `${SOLID_RESOURCE_URL.SUB_COM}#${subComponent[i].subIdentifier}`
       )
     ) {
-       // Debug
-       console.log("Thing has been created");
-       //
+      
       thingToAdd = createThing({ name: subComponent[i].subIdentifier });
+      componentsThatBelongTo = [];
 
       for (let key in SOLID_SUB_PROPERTY) {
         thingToAdd = addStringNoLocale(
@@ -60,32 +61,29 @@ const adjustSubDataSet = async (subComponent, mainComponent) => {
       }
       thingToAdd = addUrl(thingToAdd, RDF.type, SOLID_TYPES.PRODUCT);
     } else {
-      // Debug
-      console.log("Thing has been gotten");
-      //
+     
       thingToAdd = getThing(
         componentSolidDataSet,
         `${SOLID_RESOURCE_URL.SUB_COM}#${subComponent[i].subIdentifier}`
       );
+      componentsThatBelongTo = JSON.parse(getStringNoLocale(thingToAdd, SOLID_TYPES.PART_OF))
+      
     }
-    // Debug
-    console.log("Thing is:");
-    console.log(thingToAdd);
-    //
     thingToAdd = buildThing(thingToAdd)
-      .addStringNoLocale(
+      .setStringNoLocale(
         SOLID_TYPES.PART_OF,
-        JSON.stringify({
+        JSON.stringify([...componentsThatBelongTo, {
           name: mainComponent.productName,
-          identifer: mainComponent.identifer,
-        })
+          identifer: mainComponent.identifer
+        }])
       )
       .build();
-
-    // Debug
-    console.log("Thing after modification:");
-    console.log(thingToAdd);
-    //
+    /**
+     {
+          name: mainComponent.productName,
+          identifer: mainComponent.identifer,
+        }
+     */
 
     componentSolidDataSet = setThing(componentSolidDataSet, thingToAdd);
     const savedSolidDataSet = await saveSolidDatasetAt(
@@ -98,7 +96,13 @@ const adjustSubDataSet = async (subComponent, mainComponent) => {
 
 export const addComponent = async (component) => {
   let componentSolidDataSet = await initiatDataSet(SOLID_RESOURCE_URL.MAIN_COM);
+  // Validation check if the main components already exsited
 
+  if (isThingExsists(componentSolidDataSet, `${SOLID_RESOURCE_URL.MAIN_COM}#${component.identifer}`)) {
+    return {
+      success: false, thingExisted: true
+    }
+  }
   let componentThing = createThing({ name: component.identifer });
   for (let key in SOLID_MAIN_PROPERTY) {
     componentThing = addStringNoLocale(
